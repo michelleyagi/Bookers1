703e748ceb57bc3e3636086ebb3aed7094c0c726
NorimitsuSuga <149935006+michelleyagi@users.noreply.github.com>
2023-12-31 01:20:35 +0000

    neme付ルート作成
    routes.rb編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  resources :books
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book_show'
+  post 'books/:id/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
